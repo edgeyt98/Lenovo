6259cfcc73bb1d6320500473441777ae33d4f2ff
Jinendra <jinendra09082006@gmail.com>
2024-06-30 14:39:46 +0530

    common.js is commited

---
+++ common.js
@@ -0,0 +1,9 @@
+let cardData =[
+    {url:'lenovogamingpc.jpg',name:'Lenovo Legion Gaming PC',actualPrice:'10,00,000',discountPrice:'5,00,000',id:1},
+    {url:'lenovocpu.jpg',name:'Lenovo Thinkcentre PC',actualPrice:'1,00,000',discountPrice:'50,000',id:2},
+    {url:'lenovotab.jpg',name:'Wifi Only Android 11 Lenovo Tab M10 3rd GEN FHD - TB-328 FU , Accelerometer, Screen Size: 10.1',actualPrice:'20,000',discountPrice:'14,999',id:3},
+    {url:'lenovogaming.jpg',name:'Lenovo Thinkcentre gaming PC',actualPrice:'1,50,000',discountPrice:'80,000',id:4},
+    {url:'lenovolaptop.jpg',name:'ThinkPad P16s 40.64cms - 13th Gen Intel i7',actualPrice:'1,60,999',discountPrice:'1,39,699',id:5}
+];
+let cardNumber =0;
+let cardNumber2 = 0;
